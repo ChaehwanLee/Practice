ff3f4f75151cc189e9d7bc43461f76394cc55cfc
Chaehwan Lee <oasisinyourmind@gmail.com>
2022-02-19 23:27:04 +0900

    "Add WHERE, condition,"

--- mysql3.sql
+++ mysql3.sql
@@ -27,3 +27,13 @@ SELECT first_name FROM employees; -- 필요로 하는 열만 가져오기 employ
 SELECT f_name, l_name, s, FROM employees; -- 여러개의 열을 가져오는 것
 
 -- 주석을 사용할 때는 -- 이후에 한 칸 띄우고 사용, 여러줄 주석 /* */ 으로 묶음
+
+SELECT 필드이름 FROM 테이블이름 WHERE 조건식;
+
+USE sqlDB;
+SELECT * FROM userTable; -- userTalbe 에 모든 열을 가져오라고 하면 데이터가 너무 많을 수 있음
+
+SELECT * FROM userTable WHERE name = '김사부'; -- userTable의 이름이 김사부 인 사람을 가져와라
+
+SELECT * FROM userID, name FROM userTable WHERE byear >= 1960 AND height >= 170;
+SELECT * FROM userID, name FROM userTable WHERE byear >= 1960 OR height >= 170;
