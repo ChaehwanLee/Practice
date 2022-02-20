be7de68abc5171da7602382064eaa3f53e9cf568
Chaehwan Lee <oasisinyourmind@gmail.com>
2022-02-21 00:05:18 +0900

    "Add <"

--- p3.py
+++ p3.py
@@ -2,10 +2,26 @@
 #  if age < 19:
 #      print("애들은 가라")
 
-a = 2
-if a == 2:
-    print("2이다")
-if a > 5:
-    print("5보다 크다")
-if a < 5:
-    print("5보다 작다")
+# a = 2
+# if a == 2:
+#     print("2이다")
+# if a > 5:
+#     print("5보다 크다")
+# if a < 5:
+#     print("5보다 작다")
+
+# country = "Korea"
+# if country == "Korea":
+#     print("한국인")
+# if country == " Korea":
+#     print("한국사람")
+
+# energy = 1
+# if energy:
+#     print("열심히 싸운다")
+
+# p
+
+a = 3
+if 1 < a < 10:
+    print("Y")
