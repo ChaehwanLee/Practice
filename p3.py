ce6e0bf8d53806178056f4bb3e9ddddb653bf6e7
Chaehwan Lee <oasisinyourmind@gmail.com>
2022-02-20 23:54:21 +0900

    "Add int, if"

---
+++ p3.py
@@ -0,0 +1,11 @@
+# age = int(input("How old are you?"));
+#  if age < 19:
+#      print("애들은 가라")
+
+a = 2
+if a == 2:
+    print("2이다")
+if a > 5:
+    print("5보다 크다")
+if a < 5:
+    print("5보다 작다")
