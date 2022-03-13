04d484e36cf86aa0e0b4da30bd808f90b0cb7110
Chaehwan <oasisinyourmind@gmail.com>
2022-03-13 19:38:46 +0900

    "Add html"

---
+++ p2.py
@@ -0,0 +1,28 @@
+# for y in range(1, 10):
+#     for x in range(y):
+#         print("*", end="")
+#     print()
+    
+
+# while 조건:
+#    명령
+
+# std = 1
+# while std <= 5:
+#     print(std, "의 횟수를 구합니다.")
+#     std += 1
+
+
+# num = 1
+# sum = 0
+# while num <= 100:
+#     sum += num
+#     num += 1
+# print("sum=", sum)
+
+num = 151
+sum = 0
+while num <= 300:
+    sum += num
+    num += 2
+print("sum=",sum)
--- p3.py
+++ p3.py
@@ -37,7 +37,10 @@
 # print("공부는 나의 일")
 
 
-sum = 0
-for num in range(2, 101, 2):
-    sum += num
-print('sum =', sum)
+# sum = 0
+# for num in range(2, 101, 2):
+#     sum += num
+# print('sum =', sum)
+
+
+
