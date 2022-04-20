44e60072f205aea486b87f2dd790fedc137af93c
Chaehwan <oasisinyourmind@gmail.com>
2022-04-20 21:43:25 +0900

    RPi.GPIO

---
+++ p4.py
@@ -0,0 +1,19 @@
+import PRi.GPIO as gpio
+import time
+pir_pin = 27
+gpio.setmode(gpio.BCM)
+gpio.setup(pir_pin,gpio.IN)
+
+try:
+    while True:
+        if gpio.input(pir_pin == 1:
+            print("motion detected....")
+        else:
+            print("no motion....")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    gpio.clenup()
