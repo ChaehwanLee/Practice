63f7268c5940b320dc74ad2ca36e0d43dd2f7899
Chaehwan <oasisinyourmind@gmail.com>
2022-05-31 01:08:30 +0900

    practice react1

---
+++ react1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View style={{ flex: 2, justifyContent: "center", alignItems: "center" }}>
+      <Text>
+        안녕하세요 리액트 기본입니다 ! 🎉
+      </Text>
+    </View>
+  );
+}
+
+export default YourApp;
